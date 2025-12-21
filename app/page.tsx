93f864210f5b9632e0ee011b53ff482d6e40194a
rahscripts <rahmantjis@gmail.com>
2025-12-21 17:31:53 +0530

    updated favicon and mistakes done

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <section>
         <div className="max-w-3xl mx-auto m-20 p-10">
-          <div className="flex flex-col items-baseline items-center justify-center">
-          <h1 className="text-3xl font-bold ">Gym Calculator <span className="uppercase font-black tracking-tighter text-xs italic underline decoration-wavy text-green-600">by MR</span> </h1><br />
+          <div className="flex flex-col items-center justify-center">
+          <h1 className="text-3xl font-extrabold ">Gym Calculator <span className="uppercase font-black tracking-tighter text-xs italic underline decoration-wavy text-green-600">by MR</span> </h1><br />
           <button className="bg-green-500 p-2 rounded font-bold hover:bg-green-600 cursor-pointer" onClick={() => setStart(!start)}>Start</button>
                 </div>
                 {start && 
@@ -19,7 +19,7 @@ export default function Home() {
           <div className="inset-10 bg-green-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white -space-y-2">
             <h1 className="text-3xl italic">Choose: </h1>
             <button onClick={() => {setSkinny(!skinny); setStart(!start)}} className="bg-green-500 p-1 mt-4 m-2 rounded text-black cursor-pointer">Skinny</button>
-            <button onClick={() => {setSkinny(!skinny); setStart(!start)}} className="bg-green-500 p-1 m-1 rounded text-black cursor-pointer">Fatty</button>
+            <button onClick={() => {setFatty(!fatty); setStart(!start)}} className="bg-green-500 p-1 m-1 rounded text-black cursor-pointer">Fatty</button>
           </div>
           </div>
           }
