4c992a0c61b9ed329546c7d51347845dbf5d600a
rahscripts <rahmantjis@gmail.com>
2025-12-21 17:26:38 +0530

    styling and login done

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-green-100 tracking-tight scroll-smooth`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <section>
         <div className="max-w-3xl mx-auto m-20 p-10">
-          <div>
-          Gym Calculator: <br />
-          <button onClick={() => setStart(!start)}> {start ? 'start' : 'not started'}</button>
+          <div className="flex flex-col items-baseline items-center justify-center">
+          <h1 className="text-3xl font-bold ">Gym Calculator <span className="uppercase font-black tracking-tighter text-xs italic underline decoration-wavy text-green-600">by MR</span> </h1><br />
+          <button className="bg-green-500 p-2 rounded font-bold hover:bg-green-600 cursor-pointer" onClick={() => setStart(!start)}>Start</button>
                 </div>
                 {start && 
                 <div>
@@ -25,17 +25,19 @@ export default function Home() {
           }
           <div>
             {skinny &&
-            <div className="inset-10 bg-blue-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white -space-y-2">
+            <div className="inset-10 bg-blue-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white space-y-2">
                if you go gym for 2 years you will look like:
-               <Image src="/ronaldo.webp" alt="your image" height={100} width={100}/>
+               <Image src="/ronaldo.webp" alt="your image" height={500} width={500}/>
+<button className="bg-blue-500 cursor-pointer hover:bg-blue-600 p-1 rounded " onClick={() => {setStart(!start); setSkinny(!skinny)}}>Go Back!</button>
             </div>
             }
           </div>
           <div>
             {fatty &&
-             <div className="inset-10 bg-blue-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white -space-y-2">
+             <div className="inset-10 bg-blue-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white space-y-2">
                if you go gym for 2 years you will look like:
-               <Image src="/Sam Sulek.jpeg" alt="your image" height={100} width={100}/>
+               <Image src="/Sam Sulek.jpeg" alt="your image" height={500} width={500}/>
+              <button className="bg-blue-500 cursor-pointer hover:bg-blue-600 p-1 rounded " onClick={() => {setStart(!start); setFatty(!fatty)}}>Go Back!</button>
             </div>
             }
           </div>
