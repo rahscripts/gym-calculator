b8dd46c812105c88ea4ae792065843bbf691372d
rahscripts <rahmantjis@gmail.com>
2025-12-21 17:17:34 +0530

    frame of books calculator done

--- app/page.tsx
+++ app/page.tsx
@@ -9,23 +9,38 @@ export default function Home() {
 
   return (
     <section>
-      <div>
-        Gym Calculator: <br />
-        <button onClick={() => setStart(!start)}> {start ? 'start' : 'not started'}</button>
-      </div>
-      {start && 
-      <div>
-        <div className="inset-10 bg-red-600 p-20 rounded m-20 flex flex-col items-center font-bold text-white ">
-          Choose: 
-          <button onClick={() => setSkinny(!skinny)} className="bg-green-500 p-2 m-3 rounded text-black cursor-pointer">Skinny</button>
-          <button onClick={() => setFatty(!skinny)} className="bg-green-500 p-2 m-3 rounded text-black cursor-pointer">Fatty</button>
+        <div className="max-w-3xl mx-auto m-20 p-10">
+          <div>
+          Gym Calculator: <br />
+          <button onClick={() => setStart(!start)}> {start ? 'start' : 'not started'}</button>
+                </div>
+                {start && 
+                <div>
+          <div className="inset-10 bg-green-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white -space-y-2">
+            <h1 className="text-3xl italic">Choose: </h1>
+            <button onClick={() => {setSkinny(!skinny); setStart(!start)}} className="bg-green-500 p-1 mt-4 m-2 rounded text-black cursor-pointer">Skinny</button>
+            <button onClick={() => {setSkinny(!skinny); setStart(!start)}} className="bg-green-500 p-1 m-1 rounded text-black cursor-pointer">Fatty</button>
+          </div>
+          </div>
+          }
+          <div>
+            {skinny &&
+            <div className="inset-10 bg-blue-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white -space-y-2">
+               if you go gym for 2 years you will look like:
+               <Image src="/ronaldo.webp" alt="your image" height={100} width={100}/>
+            </div>
+            }
+          </div>
+          <div>
+            {fatty &&
+             <div className="inset-10 bg-blue-700 p-10 rounded m-20 flex flex-col items-center font-bold text-white -space-y-2">
+               if you go gym for 2 years you will look like:
+               <Image src="/Sam Sulek.jpeg" alt="your image" height={100} width={100}/>
+            </div>
+            }
+          </div>
         </div>
-        </div>
-        }
-        <div>
-           if you go gym for 2 years you will look like:
-           <Image src="/ronaldo.webp" alt="your image" height={100} width={100}/>
-        </div>
+        
     </section>
   );
 }
